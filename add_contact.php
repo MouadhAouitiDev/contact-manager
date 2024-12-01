8a73dfeccd5735ffed797ea67864b8a24e37271b
Mouadh Aouiti <aouitimouadh@gmail.com>
2024-12-02 00:28:19 +0100

    Public on github page

--- add_contact.php
+++ add_contact.php
@@ -44,6 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/style.css" rel="stylesheet" >
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" >
     <title>Ajouter un Contact</title>
 </head>
